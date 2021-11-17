0fa1601b1f3ed971b19c3bd3bc9a2edbddaf069e
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-17 21:38:22 +0100

    Python 3 compatibility. Using simpler requests library.

--- README.md
+++ README.md
@@ -2,6 +2,26 @@
 
 This script uses UPnP to reconnect the FRITZ!Box. Tested with FRITZ!OS 7.12
 
+## Preparation
+
+Either install the `requests` library manually by running `pip install requests`. Or in case you have `pipenv` installed run `pipenv install` in the project folder (ideally in a _virtualenv_ managed by `pyenv`):
+
+    $ pipenv install
+    Courtesy Notice: Pipenv found itself running within a virtual environment, so it will automatically use that environment, instead of creating its own for any project. You can set PIPENV_IGNORE_VIRTUALENVS=1 to force pipenv to ignore that environment and create its own instead. You can set PIPENV_VERBOSITY=-1 to suppress this warning.
+    Pipfile.lock (b1592e) out of date, updating to (a290a1)...
+    Locking [dev-packages] dependencies...
+    Locking [packages] dependencies...
+    Building requirements...
+    Resolving dependencies...
+    ✔ Success!
+    Updated Pipfile.lock (a290a1)!
+    Installing dependencies from Pipfile.lock (a290a1)...
+    🐍   ▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉▉ 4/4 — 00:00:00
+    To activate this project's virtualenv, run pipenv shell.
+    Alternatively, run a command inside the virtualenv with pipenv run.
+
+## Execution
+
 ```
 % python reconnect.py
 <?xml version="1.0" encoding="utf-8"?>
--- reconnect.py
+++ reconnect.py
@@ -1,31 +1,40 @@
 import requests
 
-soap_body = (
-    '<?xml version="1.0" encoding="utf-8"?>'
-    '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">'
-    '  <s:Body>'
-    '    <u:ForceTermination xmlns:u="urn:schemas-upnp-org:service:WANIPConnection:2"/>'
-    '  </s:Body>'
-    '</s:Envelope>'
-)
-soap_body_raw = bytes(soap_body, encoding='utf-8')
 
-soap_action = "urn:schemas-upnp-org:service:WANIPConnection:2#ForceTermination"
+def reconnect():
 
-headers = {
-    'SOAPAction': f'"{soap_action}"',
-    'Host': 'fritz.box:49000',
-    'Content-Type': 'text/xml',
-    'Content-Length': str(len(soap_body_raw))
-}
+    soap_body = (
+        '\r\n'
+        '<?xml version="1.0" encoding="utf-8"?>'
+        '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">'
+        '  <s:Body>'
+        '    <u:ForceTermination xmlns:u="urn:schemas-upnp-org:service:WANIPConnection:2"/>'
+        '  </s:Body>'
+        '</s:Envelope>'
+    )
+    soap_body_raw = bytes(soap_body, encoding='utf-8')
 
-ctrl_url = "http://fritz.box:49000/igd2upnp/control/WANIPConn1"
+    soap_action = "urn:schemas-upnp-org:service:WANIPConnection:2#ForceTermination"
 
-response = requests.request(
-    method='get',
-    url=ctrl_url,
-    data=soap_body_raw,
-    headers=headers
-)
+    headers = {
+        'SOAPAction': f'{soap_action}',
+        'Host': 'fritz.box:49000',
+        'Content-Type': 'text/xml; charset=utf-8',
+        'Content-Length': str(len(soap_body_raw))
+    }
 
-print(response.text)
+    ctrl_url = "http://fritz.box:49000/igd2upnp/control/WANIPConn1"
+
+    return requests.request(
+        method='post',
+        url=ctrl_url,
+        data=soap_body_raw,
+        headers=headers
+    )
+
+
+if __name__ == '__main__':
+    response = reconnect()
+    print(response.text)
+    if 200 != response.status_code:
+        exit(response.status_code)
