2e9778219d22ca313913f8dd22ba6ddd632acd9c
Henrik Sachse <56216308+0x7d7b@users.noreply.github.com>
2021-11-17 21:11:19 +0100

    Python 3 compatibility. Using simpler requests library.

--- reconnect.py
+++ reconnect.py
@@ -1,26 +1,31 @@
-import urllib2
+import requests
 
-soap_body = '\r\n'.join((
-    '<?xml version="1.0" encoding="utf-8"?>',
-    '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">',
-    '  <s:Body>',
-    '    <u:ForceTermination xmlns:u="urn:schemas-upnp-org:service:WANIPConnection:2"/>',
-    '  </s:Body>',
-    '</s:Envelope>'))
-
+soap_body = (
+    '<?xml version="1.0" encoding="utf-8"?>'
+    '<s:Envelope s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:s="http://schemas.xmlsoap.org/soap/envelope/">'
+    '  <s:Body>'
+    '    <u:ForceTermination xmlns:u="urn:schemas-upnp-org:service:WANIPConnection:2"/>'
+    '  </s:Body>'
+    '</s:Envelope>'
+)
+soap_body_raw = bytes(soap_body, encoding='utf-8')
 
 soap_action = "urn:schemas-upnp-org:service:WANIPConnection:2#ForceTermination"
 
 headers = {
-    'SOAPAction': u'"%s"' % (soap_action),
-    'Host': u'fritz.box:49000',
+    'SOAPAction': f'"{soap_action}"',
+    'Host': 'fritz.box:49000',
     'Content-Type': 'text/xml',
-    'Content-Length': len(soap_body),
+    'Content-Length': str(len(soap_body_raw))
 }
 
 ctrl_url = "http://fritz.box:49000/igd2upnp/control/WANIPConn1"
 
-request = urllib2.Request(ctrl_url, soap_body, headers)
-response = urllib2.urlopen(request)
+response = requests.request(
+    method='get',
+    url=ctrl_url,
+    data=soap_body_raw,
+    headers=headers
+)
 
-print response.read()
+print(response.text)
